12b193a8bfe34a68ef0e11cfbb1bd507982275e1
Vishal Saini <vishalsaini1602@gmail.com>
2022-09-24 12:42:09 +0530

    Updates

---
+++ precache-manifest.a73f89092b44da1571de387d36dad05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35858d1374408db4bfeddfc45d77efc7",
+    "url": "/ToDo_List/index.html"
+  },
+  {
+    "revision": "45616542e9ed84e27a20",
+    "url": "/ToDo_List/static/css/main.ee5d6995.chunk.css"
+  },
+  {
+    "revision": "58e9e32489f205a49016",
+    "url": "/ToDo_List/static/js/2.a6507c3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ToDo_List/static/js/2.a6507c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45616542e9ed84e27a20",
+    "url": "/ToDo_List/static/js/main.fdf91598.chunk.js"
+  },
+  {
+    "revision": "3b88745f32a466bf9ec2",
+    "url": "/ToDo_List/static/js/runtime-main.2d226fc8.js"
+  }
+]);
